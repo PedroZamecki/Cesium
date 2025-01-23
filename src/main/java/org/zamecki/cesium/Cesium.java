230a889365d3444e61b89568aca51b9cce2a4c02
PedroZamecki <pedrozamecki@gmail.com>
2025-01-23 10:48:32 -0300

    first commit

---
+++ src/main/java/org/zamecki/cesium/Cesium.java
@@ -0,0 +1,10 @@
+package org.zamecki.cesium;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cesium implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
